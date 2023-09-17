456043362a140711839d5f054f74d5fc4abb205f
Aeron01 <one.soulreaper@gmail.com>
2023-09-17 14:39:04 +0200

    solve missing keys ID for each src images in slider :construction:

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -29,7 +29,7 @@ const Slider = () => {
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img key={event.index} src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
