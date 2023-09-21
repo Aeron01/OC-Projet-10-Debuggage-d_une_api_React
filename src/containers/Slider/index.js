594dd5da2ed105c2dd68d14ec071f3018db52757
Aeron01 <one.soulreaper@gmail.com>
2023-09-21 14:58:25 +0200

    fixed the issue of months not displaying correctly in the carousel :bug: :construction:

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -17,16 +17,8 @@ const Slider = () => {
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
     new Date(evtA.date) - new Date(evtB.date)
   );
-
-  /* let byDateDescLength = 0;
-  if (byDateDesc.length === undefined) {
-    byDateDescLength = 1;
-  } else {
-    byDateDescLength = byDateDesc.length;
-  } */
-  
+ 
   const nextCard = () => {
-    // console.log(data)
     setTimeout(
       () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
@@ -42,12 +34,12 @@ const Slider = () => {
       {byDateDesc?.map((event, idx) => (
         <>
           <div
-            key={event.title}
+            key={event.id}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img key={event.index} src={event.cover} alt="forum" />
+            <img key={event.cover} src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
