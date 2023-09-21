cde5ea7503ce0e4275fa250b277e1419027ea0fa
Aeron01 <one.soulreaper@gmail.com>
2023-09-21 17:48:05 +0200

    fixed a little mistake in slider index.js file :construction:

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -15,7 +15,7 @@ const Slider = () => {
   }
 
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) - new Date(evtB.date)
+    new Date(evtA.date) - new Date(evtB.date) ? -1 : 1
   );
  
   const nextCard = () => {
