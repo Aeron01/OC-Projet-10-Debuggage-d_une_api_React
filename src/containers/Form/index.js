29624e4ef5748543098801bd777605121a4b2106
Aeron01 <one.soulreaper@gmail.com>
2023-09-25 15:50:26 +0200

    Fixes issue where sending confirmation of message does not appear. :bug:

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // Fixes issue where sending confirmation of message does not appear.
       } catch (err) {
         setSending(false);
         onError(err);
